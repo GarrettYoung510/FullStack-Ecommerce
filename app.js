9d4a0f948d0d53b8d86d89a9dfd7eaf0a0005512
Garrett Young <garrettyoung510@gmail.com>
2019-08-28 18:36:44 -0700

    testing deployment

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+// ... other imports 
+const path = require("path")
+
 // allows us to use .env
 require("dotenv").config();
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
 
-app.get("/", (req, res) => {
-    // sends to display on browser
-  res.send("hello from node");
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(`DB Connected`));
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
+
+const port = process.env.PORT || 5000;
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-const port = process.env.PORT || 8000;
 
 // run it
 app.listen(port, () => {
